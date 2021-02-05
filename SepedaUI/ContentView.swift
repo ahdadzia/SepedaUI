2e7897b935df32ee14c906250ae2b1d812a7bf2e
ahdadzia <ahda.fakhri@gmail.com>
2021-02-05 11:04:50 +0700

    menambahkan fitur tambahKeranjang

--- SepedaUI/ContentView.swift
+++ SepedaUI/ContentView.swift
@@ -46,13 +46,15 @@ struct ContentView: View {
         ProductModel(id: 10, namaProduk: "Pacific", fotoProduk: "foto10", hargaProduk: 2000000, lokasi: "Kab. Sumedang", ratingCount: 4, jumlahRating: 56)
     ]
     
+    @State var jumlahKeranjang: Int = 0
+    
     var body: some View {
         //        Product()
         NavigationView{
             ScrollView{
                 ForEach(data){ row in
                     VStack(spacing: 10){
-                        Product(data: row)
+                        Product(jumlahProduk: self.$jumlahKeranjang, data: row)
                     }
                     .padding()
                 }
@@ -65,14 +67,35 @@ struct ContentView: View {
                         Image(systemName: "person.fill")
                     }
                     
-                    Button(action: {print("")}){
-                        Image(systemName: "cart.fill")
-                    }
+                    keranjangView(jumlah: $jumlahKeranjang)
                 }
             )
         }
         .accentColor(Color.secondary)
         .navigationViewStyle(StackNavigationViewStyle())
+    }
+}
+
+struct keranjangView: View {
+    
+    @Binding var jumlah: Int
+    
+    var body: some View{
+        ZStack{
+            Button(action: {print("")}){
+                Image(systemName: "cart.fill")
+                    .resizable()
+                    .frame(width: 20, height: 20)
+            }
+            Text("\(jumlah)")
+                .foregroundColor(.white)
+                .frame(width: 10, height: 10)
+                .font(.body)
+                .padding(5)
+                .background(Color.red)
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                .offset(x: 10, y: -10)
+        }
     }
 }
  
@@ -83,6 +106,8 @@ struct ContentView_Previews: PreviewProvider {
 }
  
 struct Product: View {
+    
+    @Binding var jumlahProduk: Int
     
     let data: ProductModel
     
@@ -136,24 +161,34 @@ struct Product: View {
             .padding(.trailing)
             .padding(.top, 5)
             
-            Button(action: {print("ditambahkan")}){
-                HStack{
-                    Spacer()
-                    HStack{
-                        Image(systemName: "cart")
-                        Text("Tambah ke keranjang")
-                            .font(.callout)
-                            .padding()
-                    }
-                    Spacer()
-                }
-            }
-            .background(Color.green)
-            .foregroundColor(Color.white)
-            .cornerRadius(10)
-            .padding()
+            tambahKeranjang(jumlah: $jumlahProduk)
+            
         }
         .background(Color("warna"))
         .cornerRadius(15)
+    }
+}
+
+struct tambahKeranjang: View {
+    
+    @Binding var jumlah: Int
+    
+    var body: some View{
+        Button(action: {self.jumlah += 1}){
+            HStack{
+                Spacer()
+                HStack{
+                    Image(systemName: "cart")
+                    Text("Tambah ke keranjang")
+                        .font(.callout)
+                        .padding()
+                }
+                Spacer()
+            }
+        }
+        .background(Color.green)
+        .foregroundColor(Color.white)
+        .cornerRadius(10)
+        .padding()
     }
 }
